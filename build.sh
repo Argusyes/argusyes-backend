a6b16b8a8ff87b24337fbd269f524e7ab48b79d2
cc13446 <1344613920@qq.com>
2022-11-09 20:15:39 +0800

    feat(build): optimize build script

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 GOOS=linux GOARCH=arm64 GOARM=7 go build -o target/linux/arm/argusyes-backend ./*.go
+cp -f ./conf.toml target/linux/arm/conf.toml
 GOOS=linux GOARCH=amd64 go build -o target/linux/amd/argusyes-backend ./*.go
+cp -f ./conf.toml target/linux/amd/conf.toml
 GOOS=darwin GOARCH=arm64 GOARM=7 go build -o target/darwin/arm/argusyes-backend ./*.go
+cp -f ./conf.toml target/darwin/arm/conf.toml
 GOOS=darwin GOARCH=amd64 go build -o target/darwin/amd/argusyes-backend ./*.go
+cp -f ./conf.toml target/darwin/amd/conf.toml
